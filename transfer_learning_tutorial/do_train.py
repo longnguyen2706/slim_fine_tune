d28621d0f48da0d4c652c761577e6801959f7c9b
Long Nguyen <ndlong95@gmail.com>
2018-06-25 22:50:56 +0800

    add training command

--- transfer_learning_tutorial/do_train.py
+++ transfer_learning_tutorial/do_train.py
@@ -218,7 +218,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--finetune_learning_rate',
         # nargs='+',
-        default=0.1,
+        default=0.2,
         type=float
     )
 
